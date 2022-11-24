df7997bf64609d56d959815c9cbc349723fb5fa2
Bobur <bobur927788575@outlook.com>
2022-11-25 02:53:45 +0500

    n

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -12,12 +12,16 @@ public class Animal {
     }
 
     public String getDescription() {
-        if (hasFur) {
+        if (hasFur && numberOfPaws == 1){
+            return "This animal is mostly " + color + ". It has " + numberOfPaws
+                    + " paw and a fur.";
+        }else if (hasFur && numberOfPaws > 1){
             return "This animal is mostly " + color + ". It has " + numberOfPaws
                     + " paws and a fur.";
+        }else {
+            return "This animal is mostly " + color + ". It has " + numberOfPaws
+                    + " paws and no fur.";
         }
-        return "This animal is mostly " + color + ". It has " + numberOfPaws
-                + " paws and no fur.";
     }
 }
 
